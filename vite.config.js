a39fbebc6ccfb93ba81811842ab7667cc7e0370a
orochi85 <brunojaeh2@gmail.com>
2025-02-28 00:40:42 -0300

    Corrigir sintaxe do vite.config.js para export único

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import EnvironmentPlugin from 'vite-plugin-environment';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    EnvironmentPlugin(['VITE_API_KEY', 'VITE_API_URL', 'VITE_WEATHER_API_KEY']), // Adicione suas variáveis de ambiente aqui
-  ],
-  base: '/nome-do-repositorio/', // Substitua pelo nome do seu repositório
+  plugins: [react()], // Plugin para React
+  build: {
+    rollupOptions: {
+      external: ['@rollup/rollup-linux-x64-gnu'], // Ignora o módulo nativo
+    },
+  },
 });
